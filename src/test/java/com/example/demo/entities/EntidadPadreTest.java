0d4a1502aacffef63e6d511510e5a4ca88306f62
Alejandro <mail.alejandro.teixeira@gmail.com>
2025-03-08 17:33:35 +0100

    1st commit

--- src/test/java/com/example/demo/entities/EntidadPadreTest.java
+++ src/test/java/com/example/demo/entities/EntidadPadreTest.java
@@ -3,6 +3,9 @@ package com.example.demo.entities;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,7 +13,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Log4j2
 @DataJpaTest
 @Testcontainers
-public class EntidadPadreTest {
+class EntidadPadreTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(
+            "mysql:latest"
+    ).withDatabaseName("testDB").withUsername("appuser").withPassword("password123");
 
     @Test
     void testEntidadPadreConstructorWithNombre() {
