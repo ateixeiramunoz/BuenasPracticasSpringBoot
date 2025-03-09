aad84e37fe8e89093a00b4d2d8375664ed57ed3a
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-09 05:58:34 +0100

    1st commit

--- src/test/java/com/example/demo/controllers/HomeControllerTest.java
+++ src/test/java/com/example/demo/controllers/HomeControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.controllers;
 
 import com.example.demo.MyBaseIntegrationTest;
-import com.example.demo.repositories.EntidadPadreRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -11,12 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
--- src/test/java/com/example/demo/entities/EntidadPadreTest.java
+++ src/test/java/com/example/demo/entities/EntidadPadreTest.java
@@ -2,12 +2,10 @@ package com.example.demo.entities;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
