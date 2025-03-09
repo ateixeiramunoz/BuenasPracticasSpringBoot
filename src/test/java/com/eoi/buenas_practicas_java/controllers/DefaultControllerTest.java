9c8f6e27542f687a485a4afd0418783dd7841454
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-09 17:26:37 +0100

    1st commit

--- src/test/java/com/eoi/buenas_practicas_java/controllers/DefaultControllerTest.java
+++ src/test/java/com/eoi/buenas_practicas_java/controllers/DefaultControllerTest.java
@@ -13,9 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import com.eoi.buenas_practicas_java.services.EntidadHijaService;
-
 
 import java.util.Base64;
 
